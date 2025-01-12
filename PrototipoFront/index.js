e86c49cb4990efb6bc53a7a027ae89a64c4f4ad8
Tgomes18 <thales.batista@estudante.ufjf.br>
2025-01-12 16:00:46 -0300

    só mais alguns detalhes e enfim Icon

--- PrototipoFront/index.js
+++ PrototipoFront/index.js
@@ -3,13 +3,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     // Função para mostrar o conteúdo exclusivo
     function mostrarConteudo(id) {
-        // Esconde todos os conteúdos
         var conteudos = document.querySelectorAll('.conteudo');
         conteudos.forEach(conteudo => {
             conteudo.classList.remove('mostrar');
         });
         
-        // Mostra o conteúdo específico
         var conteudoExclusivo = document.getElementById(id);
         conteudoExclusivo.classList.add('mostrar');
     }
@@ -45,6 +43,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+    // Função para remover itens da lista
     function removerItem() {
         console.log('Função removerItem chamada');
         if (itemSelecionado) {
@@ -76,12 +75,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         mostrarConteudo('conteudoPedidos');
     });
 
-    // Evento para o botão Logout
     document.getElementById('Logout').addEventListener('click', () => {
         window.location.href = 'login.html';
     });
 
-    // Evento para o botão Adicionar
     document.getElementById('adicionar').addEventListener('click', adicionarItem);
 
     document.getElementById('remover').addEventListener('click', removerItem);
