89cb3219f53e75b1eb37418315b82d185c767d29
mateuslfelicio <mateuslfvrb@hotmail.com>
2025-01-17 18:10:52 -0300

    adiciona login prototipo

---
+++ PrototipoFront/login.js
@@ -0,0 +1,14 @@
+document.querySelector('.formLogin').addEventListener('submit', logar);
+
+function logar(event) {
+    event.preventDefault(); //ignora envio, remover qnd for possivel validar
+    var login = document.getElementById("email").value;
+    var senha = document.getElementById("password").value;
+
+    if (login && senha) {
+        alert("sucesso");
+        location.href = "index.html";
+    } else {
+        alert("Por favor, preencha todos os campos!");
+    }
+}
