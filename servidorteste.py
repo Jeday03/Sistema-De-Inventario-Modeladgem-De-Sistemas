4144ae671c59d3c8d91be66cbefdc83f2e95eeaf
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2025-02-14 11:43:21 -0300

    Começa teste do httprequest

--- servidorteste.py
+++ servidorteste.py
@@ -1,5 +1,6 @@
-from Flask import Flask, jsonify, request
-from Flask_cors import CORS
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,16 @@ CORS(app)
 def get_random_number():
     random_number = random.randint(1, 10)
     return jsonify({'random_number': random_number})
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    if username == 'admin' and password == 'admin':
+        return jsonify({'message': 'Login efetuado'}), 200
+    else:
+        return jsonify({'message': 'Informações inválidas'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
